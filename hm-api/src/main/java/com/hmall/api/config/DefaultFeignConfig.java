74eb8d44476aa1d2e3f24d3805442db575e4cd2c
frangenlove <frangenlove@163.com>
2026-02-06 08:53:36 +0800

    完成微服务保护和分布式事务

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,5 +1,6 @@
 package com.hmall.api.config;
 
+import com.hmall.api.client.fallback.ItemClientFallbackFactory;
 import com.hmall.common.utils.UserContext;
 import feign.Logger;
 import feign.RequestInterceptor;
@@ -21,5 +22,10 @@ public class DefaultFeignConfig {
                     template.header("user-info", String.valueOf(userId));
             }
         };
+    }
+
+    @Bean
+    public ItemClientFallbackFactory itemClientFallbackFactory(){
+        return new ItemClientFallbackFactory();
     }
 }
